85bf8e40efc4f8079727f95cb255a81bad339313
aqibmushtaq <aqib@mushtaq.me>
2014-01-05 03:28:51 +0000

    Added a text field to the image activity which shows the remaining time in seconds.

--- src/iCrave/icrave/src/main/java/com/aqib/icrave/ImageFragment.java
+++ src/iCrave/icrave/src/main/java/com/aqib/icrave/ImageFragment.java
@@ -2,6 +2,8 @@ package com.aqib.icrave;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,7 +14,10 @@ import android.widget.TextView;
 
 public class ImageFragment extends Fragment {
 
-    protected AsyncTask<Integer, Integer, Integer> task;
+    private static final int UPDATE_PROGRESS_BAR = 0;
+    private static final int UPDATE_TEXT = 1;
+
+    protected AsyncTask<Void, Void, Void> task;
 
     private int progress = 0;
 
@@ -22,18 +27,47 @@ public class ImageFragment extends Fragment {
 
         ((TextView) rootView.findViewById(R.id.imageDesc)).setText("Some image description here");
         final ProgressBar countdownBar = (ProgressBar) rootView.findViewById(R.id.countdown);
+        final TextView timeRemaining = (TextView) rootView.findViewById(R.id.timeRemaining);
 
-        task = new AsyncTask<Integer, Integer, Integer>() {
-            protected Integer doInBackground(Integer... ints) {
+        final Handler uiHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                int operation = msg.arg1;
+                int value = msg.arg2;
+
+                if (operation == UPDATE_PROGRESS_BAR) {
+                    Log.d("ImageFragment", "Updating progress bar");
+                    countdownBar.setProgress(value);
+                } else if (operation == UPDATE_TEXT) {
+                    Log.d("ImageFragment", "Updating text field");
+                    timeRemaining.setText(value + "");
+                }
+            }
+        };
+
+        task = new AsyncTask<Void, Void, Void>() {
+            protected Void doInBackground(Void... params) {
                 while (progress++ < 100) {
                     if (isCancelled())
-                        return -1;
+                        return null;
                     else if (getActivity() == null) { //Ensure that the activity still exists
                         Log.d("ImageFragment", "Activity doesn't exists therefore stopping the countdown process");
                         cancel(true);
-                    } else {
+                    } else { //update the user interface to show the countdown
                         Log.d("ImageFragment", "Timer: " + progress);
-                        countdownBar.setProgress(progress);
+                        if (progress % 10 == 0) { //update the text field showing the remaining time in seconds
+                            Message m = new Message();
+                            m.arg1 = UPDATE_TEXT;
+                            m.arg2 = 10 - progress / 10;
+                            uiHandler.sendMessage(m);
+                        }
+
+                        //update the progress bar
+                        Message m = new Message();
+                        m.arg1 = UPDATE_PROGRESS_BAR;
+                        m.arg2 = progress;
+                        uiHandler.sendMessage(m);
+
                         try {
                             Thread.sleep(100);
                         } catch (InterruptedException e) {
@@ -45,13 +79,12 @@ public class ImageFragment extends Fragment {
                 getActivity().setResult(HomeFragment.RESULT_OK);
                 getActivity().finish();
                 cancel(true);
-                return 1;
+                return null;
             }
         };
         task.execute(null);
 
         return rootView;
     }
-
 
 }
