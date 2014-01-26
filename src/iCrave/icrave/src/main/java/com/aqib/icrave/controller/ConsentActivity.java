048cf958b2f235d0e2429eb88655f794864352c9
Aqib Mushtaq <aqib@mushtaq.me>
2014-01-26 09:08:29 +0000

    Moved file loading code to model package

--- src/iCrave/icrave/src/main/java/com/aqib/icrave/controller/ConsentActivity.java
+++ src/iCrave/icrave/src/main/java/com/aqib/icrave/controller/ConsentActivity.java
@@ -1,24 +1,15 @@
 package com.aqib.icrave.controller;
 
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.TextView;
 
 import com.aqib.icrave.R;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import com.aqib.icrave.model.FileLoader;
 
 public class ConsentActivity extends ActionBarActivity {
 
@@ -46,29 +37,9 @@ public class ConsentActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_consent, container, false);
-            ((TextView)rootView.findViewById(R.id.consent)).setText(readRawTextFile(getActivity().getApplicationContext(), R.raw.consent));
+            ((TextView)rootView.findViewById(R.id.consent)).setText(FileLoader.readRawTextFile(getActivity().getApplicationContext(), R.raw.consent));
             return rootView;
         }
-    }
-
-    public static String readRawTextFile(Context ctx, int resId)
-    {
-        InputStream inputStream = ctx.getResources().openRawResource(resId);
-
-        InputStreamReader inputreader = new InputStreamReader(inputStream);
-        BufferedReader buffreader = new BufferedReader(inputreader);
-        String line;
-        StringBuilder text = new StringBuilder();
-
-        try {
-            while (( line = buffreader.readLine()) != null) {
-                text.append(line);
-                text.append('\n');
-            }
-        } catch (IOException e) {
-            return null;
-        }
-        return text.toString();
     }
 
 }
---
+++ src/iCrave/icrave/src/main/java/com/aqib/icrave/model/FileLoader.java
@@ -0,0 +1,35 @@
+package com.aqib.icrave.model;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Aqib on 26/01/14.
+ */
+public class FileLoader {
+
+    public static String readRawTextFile(Context ctx, int resId)
+    {
+        InputStream inputStream = ctx.getResources().openRawResource(resId);
+
+        InputStreamReader inputreader = new InputStreamReader(inputStream);
+        BufferedReader buffreader = new BufferedReader(inputreader);
+        String line;
+        StringBuilder text = new StringBuilder();
+
+        try {
+            while (( line = buffreader.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return text.toString();
+    }
+
+}
