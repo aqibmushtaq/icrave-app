40a7146e8da3e48d7f61e3ae1ad3e88d2eee7e72
Aqib Mushtaq <aqib@mushtaq.me>
2014-04-15 07:14:06 +0100

    Added code to verify registrations using an access code

--- app/src/main/java/com/aqib/icrave/controller/LoginActivity.java
+++ app/src/main/java/com/aqib/icrave/controller/LoginActivity.java
@@ -1,11 +1,16 @@
 package com.aqib.icrave.controller;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -126,49 +131,84 @@ public class LoginActivity extends ActionBarActivity {
             });
         }
 
-        private void setRegisterListener(View rootView) {
+        private void register() {
             final Toast errorToast = Toast.makeText(getActivity().getApplicationContext(), "", Toast.LENGTH_SHORT);
 
+            Activity a = getActivity();
+            final String email = ((EditText)a.findViewById(R.id.email)).getText().toString().toLowerCase();
+            final String password = ((EditText)a.findViewById(R.id.password)).getText().toString();
+
+            if (validate(email, password, errorToast)) {
+                //successfully passed pre-request requirements
+
+                //request access code
+                final EditText accessCode = new EditText(getActivity().getApplicationContext());
+                accessCode.setInputType(InputType.TYPE_CLASS_NUMBER);
+                accessCode.setTextColor(Color.BLACK);
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Update Status")
+                        .setMessage("Access code")
+                        .setView(accessCode)
+                        .setPositiveButton(R.string.register, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                String strAccessCode = ((Editable) accessCode.getText()).toString();
+
+                                try {
+                                    if (Integer.parseInt(strAccessCode) == getResources().getInteger(R.integer.registration_access_code)) {
+                                        processRegistration(email, password, errorToast);
+                                        return;
+                                    }
+                                } catch (NumberFormatException e) { }
+
+                                //only reaches this point of an exception was thrown
+                                errorToast.setText(R.string.msg_register_access_code_incorrect);
+                                errorToast.show();
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Do nothing.
+                    }
+                }).show();
+            }
+        }
+
+        private void processRegistration(String email, String password, Toast errorToast) {
+            int result = Login.REQUEST_UNSUCCESSFUL;
+            try {
+                //attempt to register the user
+                SharedPreferences prefs = getActivity().getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                result = Login.register(getActivity().getApplicationContext(), email, password, editor);
+                Log.d("LoginActivity", String.format("Register result = %s", result));
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (result == Login.REGISTER_SUCCESSFUL) {
+                errorToast.setText(R.string.msg_register_successful);
+
+                //restart the application to refresh the main menu
+                Intent i = getActivity().getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage(getActivity().getBaseContext().getPackageName());
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            } else if (result == Login.REGISTER_UNSUCCESSFUL)
+                errorToast.setText(R.string.msg_register_unsuccessful);
+            else if (result == Login.REGISTER_EMAIL_EXISTS)
+                errorToast.setText(R.string.msg_register_email_taken);
+            else
+                errorToast.setText(R.string.error_server_not_available);
+
+            errorToast.show();
+        }
+
+        private void setRegisterListener(View rootView) {
             rootView.findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Activity a = getActivity();
-                    String email = ((EditText)a.findViewById(R.id.email)).getText().toString().toLowerCase();
-                    String password = ((EditText)a.findViewById(R.id.password)).getText().toString();
-
-
-                    if (validate(email, password, errorToast)) {
-                        //successfully passed pre-request requirements
-                        int result = Login.REQUEST_UNSUCCESSFUL;
-                        try {
-                            //attempt to register the user
-                            SharedPreferences prefs = getActivity().getSharedPreferences(PREF_FILE_NAME, MODE_PRIVATE);
-                            SharedPreferences.Editor editor = prefs.edit();
-                            result = Login.register(getActivity().getApplicationContext(), email, password, editor);
-                            Log.d("LoginActivity", String.format("Register result = %s", result));
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        if (result == Login.REGISTER_SUCCESSFUL) {
-                            errorToast.setText(R.string.msg_register_successful);
-
-                            //restart the application to refresh the main menu
-                            Intent i = getActivity().getBaseContext().getPackageManager()
-                                    .getLaunchIntentForPackage(getActivity().getBaseContext().getPackageName());
-                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(i);
-                        } else if (result == Login.REGISTER_UNSUCCESSFUL)
-                            errorToast.setText(R.string.msg_register_unsuccessful);
-                        else if (result == Login.REGISTER_EMAIL_EXISTS)
-                            errorToast.setText(R.string.msg_register_email_taken);
-                        else
-                            errorToast.setText(R.string.error_server_not_available);
-
-                        errorToast.show();
-                    }
+                    register();
                 }
             });
         }
